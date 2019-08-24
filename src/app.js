e65920aac8bef1bc0e78d704466cf825246aa6ba
Igor Souza <igorsouza.dev@gmail.com>
2019-08-24 07:39:48 -0300

    Added error handling with sentry and youch

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+    this.server.use(Sentry.Handlers.requestHandler());
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -21,6 +31,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
